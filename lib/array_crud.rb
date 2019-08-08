38dd60e4c1525620fa9124a7453a17940f761d55
Jeffrey Brown <brownjs03@gmail.com>
2019-08-08 22:04:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,37 +3,37 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["wow", "The Suffering", "am", "arrays!"]
+  ["Blood Red Summer", "The Suffering", "Welcome Home", "Black Sunday!"]
 end
 
 def add_element_to_end_of_array(array, element)
- create_an_array << "arrays!"
+ array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  arrays = create_an_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  get_out = create_an_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array[2]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  create_an_array[3]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-create_an_array[1] = "totally"
+array[1] = element
 end
